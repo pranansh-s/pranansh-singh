8a36462a84a0eec13ff8c30056da154d2b96e59a
Pranansh Singh <prananshsingh@Prananshs-MacBook-Air.local>
2023-04-27 21:10:09 +0530

    indexing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,7 @@ import Document, { Head, Html, Main, NextScript } from 'next/document';
             <link rel="icon" href="/favi.ico" type='image/x-icon' />
             <link rel="apple-touch-icon" href="/logo64.png" />
             <link rel="manifest" href="/manifest.json" />
+            <meta name="google-site-verification" content="GL83bwDes4GKEoNh1nM55lYwcAStf1a5dxKYgpvqtbQ" />
           </Head>
           <body>
             <Main />
